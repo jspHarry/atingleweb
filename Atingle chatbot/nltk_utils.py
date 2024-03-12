d8afc8853e61f374478684cc70a7c144a1bb774b
Keshav Garg <135343521+kg-06@users.noreply.github.com>
2024-03-12 19:55:07 +0530

    chatbot added directly

---
+++ Atingle chatbot/nltk_utils.py
@@ -0,0 +1,22 @@
+import nltk
+import numpy as np
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+def tokenize(sentance):
+    return nltk.word_tokenize(sentance)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentance,all_words):
+    tokenized_sentance=[stem(w) for w in tokenized_sentance]
+    bag=np.zeros(len(all_words),dtype=np.float32)
+    for idx,w in enumerate(all_words):
+        if w in tokenized_sentance:
+            bag[idx]= 1.0
+
+    return bag
+
+        
+
+
